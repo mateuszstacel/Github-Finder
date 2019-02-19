061bb5dad932428d4fffd46af8b77ae65384ca03
mateusz stacel <mateuszstacel@mateuszs-MacBook-Pro.local>
2019-02-19 13:55:05 +0000

    Fetched profile data

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// Init GitHub
+const github = new GitHub();
+
+//Search input
+
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    //Get input text
+    const userText = e.target.value;
+
+    if(userText !== '') {
+        //Make HTTP call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                //Show alert
+
+            } else {
+                //show profile
+
+            } else {
+                //Clear profile
+            }
+        })
+    }
+});
---
+++ github.js
@@ -0,0 +1,18 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'f9117242da286e6f1f60';
+        this.client_secret = '9f6564ccf0326f7042734f98925365f1f9976909';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+   
+       const profile = await profileResponse.json();
+
+       return {
+           profile: profile
+        }
+
+    }
+
+}
